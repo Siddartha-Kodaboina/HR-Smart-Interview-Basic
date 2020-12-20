59c55ff82485449ed2d7b5a5b74ea513448b6384
Steve Siddu <stevesiddu49@gmail.com>
2020-12-20 14:48:39 +0530

    coommit

---
+++ Max element in the array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+int32_t main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int ans=INT_MIN;
+    while(n--){
+        int k;
+        cin>>k;
+        ans=max(ans,k);
+    }
+    cout<<ans;
+    return 0;
+}
